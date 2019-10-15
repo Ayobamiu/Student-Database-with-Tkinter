fb76ec89413df78eeeea800c45265a7dbe1bb7bc
Ayobami Usman <49147442+Ayobamiu@users.noreply.github.com>
2019-10-15 16:19:06 +0100

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+
+class Database:
+
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+        self.conn.commit()
+
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
+        self.conn.commit()
+
+    def view(self):
+        self.cur.execute("SELECT * FROM book")
+        rows = self.cur.fetchall()
+        self.conn.commit()
+        return rows
+
+    def search(self, title="", author="", year="", isbn=""):
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
+                         (title, author, year, isbn))
+        rows = self.cur.fetchall()
+        return rows
+
+    def delete(self, id):
+        self.cur.execute("DELETE FROM book WHERE id=?", (id,))
+        self.conn.commit()
+
+    def update(self, id, title, author, year, isbn):
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",
+                         (title, author, year, isbn, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+# insert("the intro","tayo",2015, 9876576)
+# print(view())
+# #delete(3)
+# update(3,"The moon","Josh", 2017, 987654324)
+
+# print(search(author="semi"))
+# print(search(title="The moon"))
